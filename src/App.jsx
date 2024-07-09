c8a4d4aa08c2fcc143e4667b15a5c180033f98c5
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-07-09 19:13:12 +0200

    Test d'un div de flou par dessus l'image full-screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Carousel from "./components/Carousel";
 
 export default function App() {
   return (
-    <div className="w-screen h-screen bg-gray-100 overflow-hidden">
+    <div className="relative w-screen h-screen bg-gray-100 overflow-hidden">
       <Canvas>
         <Suspense fallback={null}>
           <Carousel />
         </Suspense>
       </Canvas>
+
+      <div className="absolute top-0 left-0 w-full h-full backdrop-blur-3xl"></div>
     </div>
   );
 }
