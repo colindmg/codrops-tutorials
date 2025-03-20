112edc282da14a25efd71e5ccacf327baeecb66e
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2025-03-20 18:29:00 +0100

    Grid & card initialization

--- scripts/components/Card.js
+++ scripts/components/Card.js
@@ -1,8 +1,30 @@
+import { Color, Mesh, MeshBasicMaterial, PlaneGeometry, Vector2 } from "three";
 import { ExtendedObject3D } from "../utils/ExtendedObject3D";
 
 export class Card extends ExtendedObject3D {
-  constructor() {
+  static Geometry = new PlaneGeometry(1, 1);
+
+  gridPosition = new Vector2();
+  mesh;
+
+  constructor(i, j) {
     super();
+
+    this.gridPosition.set(i, j);
+    this.#_createMesh();
+  }
+
+  #_createMesh() {
+    const r = Math.ceil(Math.random() * 255);
+    const g = Math.ceil(Math.random() * 255);
+    const b = Math.ceil(Math.random() * 255);
+
+    this.mesh = new Mesh(
+      Card.Geometry,
+      new MeshBasicMaterial({ color: new Color(`rgb(${r}, ${g}, ${b})`) })
+    );
+
+    this.add(this.mesh);
   }
 
   resize(event) {}
--- scripts/components/Grid.js
+++ scripts/components/Grid.js
@@ -1,15 +1,29 @@
 import { ExtendedObject3D } from "../utils/ExtendedObject3D";
+import { Card } from "./Card";
 
 export class Grid extends ExtendedObject3D {
+  static COLUMNS = Math.floor(window.innerWidth / 100) | 1;
+  static ROWS = Math.floor(window.innerHeight / 100) | 1;
+
   constructor() {
     super();
+
+    this.#_createCards();
   }
 
-  resize(event) {
-    
+  #_createCards() {
+    for (let i = 0; i < Grid.COLUMNS; i++) {
+      for (let j = 0; j < Grid.ROWS; j++) {
+        const card = new Card(i, j);
+        this.add(card);
+      }
+    }
   }
 
-  update(dt) {
-    
+  resize() {
+    Grid.COLUMNS = Math.floor(window.innerWidth / 100) | 1;
+    Grid.ROWS = Math.floor(window.innerHeight / 100) | 1;
   }
+
+  update(dt) {}
 }
